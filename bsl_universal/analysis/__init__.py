495268c463e328bdb76c22718510b5f41f191dac
Zhongmin <zz4@illinois.edu>
2023-12-14 21:04:34 +0800

    bug fixes for GS mantis_file

--- bsl_universal/analysis/__init__.py
+++ bsl_universal/analysis/__init__.py
@@ -1,3 +1,4 @@
 from ._mantisCam.mantis_file import mantis_file as mantis_file
 from ._mantisCam.mantis_folder import mantis_folder as mantis_folder
 from ._mantisCam.mantis_file_GS import mantis_file_GS as mantis_file_GS
+from ._mantisCam.mantis_folder_GS import mantis_folder_GS as mantis_folder_GS
---
+++ bsl_universal/analysis/_mantisCam/mantis_folder_GS.py
@@ -0,0 +1,102 @@
+from pathlib import Path
+import numpy as np
+from .mantis_file import mantis_file_GS
+from numpy.typing import NDArray
+from tqdm import tqdm
+from loguru import logger
+import re, os
+
+class mantis_folder_GS:
+    def __init__(self, path:Path, sort_with_exp:bool=False, imager_type:str="FSI", is_2x2:bool=False, origin=(1,0), R_loc=(0,1), G_loc=(1,0), B_loc=(0,0), SP_loc=(1,1), gamma:int=1):
+        """
+        Load objects points for a folder of mantisCam recording files.
+
+        Parameters
+        ----------
+        sort_with_exp : `bool`
+            Sort all video files with the exposure time encoded in its file name.
+            File name MUST contains exp time info in the format of "x.xms"
+
+        Returns
+        -------
+        mantis_folder : `mantis_folder`
+            A object with all information and data of mantisCam recoridings in the folder.
+        """
+        if type(path) is str:
+            path = Path(path)
+        self.path = path
+        self.__videos = dict()
+        self.__init_mantis_video_dict(sort_with_exp, imager_type, is_2x2, origin, R_loc, G_loc, B_loc, SP_loc, gamma)
+        
+    def __extract_time_key(self, file_name):
+        match = re.search(r'\d+\.\d+(?=ms)', file_name)
+        return float(match.group())  # Extract the float value from the time string without the 'ms' unit
+
+    def __init_mantis_video_dict(self, sort_with_exp:bool=True, imager_type:str="FSI", is_2x2:bool=False, origin=(1,0), R_loc=(0,1), G_loc=(1,0), B_loc=(0,0), SP_loc=(1,1), gamma:int=1):
+        filepaths = [f for f in self.path.iterdir() if f.is_file() and f.suffix == '.h5']
+        for filepath in filepaths:
+            self.__videos[filepath.name] = mantis_file_GS(filepath, imager_type, is_2x2, origin, R_loc, G_loc, B_loc, SP_loc, gamma)
+        if(sort_with_exp):
+            self.__videos = dict(sorted(self.__videos.items(), key=lambda item: self.__extract_time_key(item[0])))
+
+    def __getitem__(self,name:str) -> mantis_file_GS:
+        return self.__videos[name]
+
+    @property
+    def n_videos(self):
+        return len(self.__videos)
+
+    @property
+    def name_videos(self):
+        filenames = [f.name for f in self.path.iterdir() if f.is_file() and f.suffix == '.h5']
+        sorted_filenames = sorted(filenames, key=lambda f: os.stat(os.path.join(self.path, f)).st_mtime)
+        return sorted_filenames
+    
+    @property
+    def arr_files(self) -> list[mantis_file_GS]:
+        """
+        Return all the files in the mantisFolder object.
+
+        Returns
+        -------
+        arr_videos : `numpy.ndarray`
+            An array of all the video frames in all the mantisFiles.
+            in the shape of [num_files]
+        """
+        return np.array(list(self.__videos.values()))
+    
+    @property
+    def arr_videos(self) -> NDArray[np.uint16]:
+        """
+        Return all the frames in all of the files in the mantisFolder object.
+
+        Returns
+        -------
+        arr_videos : `numpy.ndarray`
+            An array of all the video frames in all the mantisFiles.
+            in the shape of [num_files, n_row, n_col, n_channels]
+        """
+        file_shape = self.arr_files[0].raw_data_shape
+        arr_size = self.n_videos *file_shape[0]*file_shape[1]*file_shape[2]*file_shape[3]*2/1000/1000/1000
+        logger.warning(f"Your are trying to load the entire folder into a single dataset, this may crush your python kernel if your RAM is not enough! \nEstimate RAM Requirement: {arr_size:.2f}GB")
+        return np.array([file.frames for file in tqdm(self.__videos.values(), desc="Loading Dataset")])
+    
+
+    def find_key(self, key:str) -> str:
+        """
+        Find the key of a file in the folder.
+
+        Parameters
+        ----------
+        key : `str`
+            The key to search for.
+
+        Returns
+        -------
+        key : `str`
+            The key of the file.
+        """
+        for file_name in self.name_videos:
+            if key in file_name:
+                return file_name
+        raise ValueError(f"Key {key} not found in folder {self.path}")
