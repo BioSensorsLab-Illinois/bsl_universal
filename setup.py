f6367ec9c461c436658075283ea41a33a0dac47d
Zhongmin Zhu <zz4@illinois.edi>
2026-02-10 01:01:25 -0600

    pypi integration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'bsl_universal',         # How you named your package folder (MyLib)
   packages = find_packages(),   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '2.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Universal Research Library for BioSensors Lab @ UIUC',   # Give a short description about your library
   author = 'Zhongmin Zhu',                   # Type in your name
